43f4e12c284f231aa4885981965906606996a553
OlinAlvarez <olinalvarez@gmail.com>
2018-03-25 15:18:16 -0700

    added promises to index.js

--- api_module/index.js
+++ api_module/index.js
@@ -13,7 +13,18 @@ exports.search = (sq) => {
     param = `search/shows?q=${sq}`
     return _fetch(param)
 }
-
+exports.singleSearch = (ss) => {
+    param = `singlesearch/shows?q=${sq}`
+    return _fetch(param)
+}
+exports.peopleSearch = (ps) =>{
+    param = `people/${ps}`
+    return _fetch(param)
+}
+exports.episodeSearch = (id) => {
+    param = `/shows/${id}/episodes`
+    return _fetch(param)
+}
 exports.fetch_by_id = (id) => {
     return _fetch(`shows/${id}`)
 }
